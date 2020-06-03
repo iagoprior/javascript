28e297e158496c9c101ab7f960ef82c54b1eb5da
Iago Prior <iagoi.a.s.p_@hotmail.com>
2020-06-03 14:54:27 -0300

    adicionando mocha

---
+++ es6avancado/src/math.js
@@ -0,0 +1,11 @@
+class Math {
+    sum(a,b, callback) {
+        setTimeout(()  => {
+            callback(a + b);
+        }, 5)
+    };
+    multiply(a,b) {
+        return a * b;
+    }
+}
+module.exports = Math;
